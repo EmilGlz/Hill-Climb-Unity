5b77edcf9b9105814f370d5c1b41cd0753133a53
Emil Guluzade <emilquluzade2001@gmail.com>
2023-10-27 10:38:19 -0400

    Added Car controller and Environment generator

---
+++ Assets/Scripts/DriveCar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveCar : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _frontTireRB;
+    [SerializeField] private Rigidbody2D _backTireRB;
+    [SerializeField] private Rigidbody2D _carRB;
+    [SerializeField] private float _speed = 150f;
+    [SerializeField] private float _rotationSpeed = 300f;
+
+    private float _moveInput;
+
+    private void Update()
+    {
+        _moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        _frontTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+        _backTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+        _carRB.AddTorque(_moveInput * _rotationSpeed * Time.fixedDeltaTime);
+    }
+
+}
---
+++ Assets/Scripts/FuelController.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelController : MonoBehaviour
+{  
+    public static FuelController Instance;
+
+    [SerializeField] private Image _fuelImage;
+    [SerializeField] private float _fuelDrainSpeed = 1f;
+    [SerializeField] private float _maxFuelAmount = 100f;
+    [SerializeField] private Gradient _gradient;
+
+    private float _currentFuelAmount;
+
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+    }
+
+    private void Start()
+    {
+        _currentFuelAmount = _maxFuelAmount;
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        _currentFuelAmount -= Time.deltaTime * _fuelDrainSpeed;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        _fuelImage.fillAmount = (_currentFuelAmount /  _maxFuelAmount);
+        _fuelImage.color = _gradient.Evaluate(_fuelImage.fillAmount);
+    }
+}
